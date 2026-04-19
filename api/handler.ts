f75ddefd5796e2a2a247b52d5a1c7d35858c31ea
Atulya <atulya2612@gmail.com>
2026-04-19 15:29:58 +0530

    Add Vercel API entrypoint

---
+++ api/handler.ts
@@ -0,0 +1,22 @@
+import adminApiHandler from "./[...route]";
+
+function buildForwardedRequest(request: Request) {
+  const currentUrl = new URL(request.url);
+  const forwardedPath = currentUrl.searchParams.get("__pathname");
+
+  if (!forwardedPath) {
+    return request;
+  }
+
+  const nextUrl = new URL(request.url);
+  nextUrl.pathname = forwardedPath;
+  nextUrl.searchParams.delete("__pathname");
+
+  return new Request(nextUrl, request);
+}
+
+export default {
+  async fetch(request: Request) {
+    return adminApiHandler(buildForwardedRequest(request));
+  },
+};
